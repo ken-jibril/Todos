dab40e98b220ed614992ce8524e9268a3beb1764
ken kagai <jibken80@gmail.com>
2025-10-18 17:14:35 +0300

    refactor: fix a typo in the h1 tag

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 function home() {
     return ( 
         <div className="">
-            <h1 className="text-4xl text-center text-blue-700">Hello the from Tailwind.CSS!</h1>
+            <h1 className="text-4xl text-center text-blue-700">Hello there from Tailwind.CSS!</h1>
         </div>
      );
 }
