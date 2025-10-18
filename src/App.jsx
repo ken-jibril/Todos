5ca7b27f82cfb30a7d59d0db110e416431b0868d
ken kagai <jibken80@gmail.com>
2025-10-18 17:27:33 +0300

    import Tanstack query and implement it

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home';
 import Navbar from './components/Navbar';
+import { useQuery, useQueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const QueryClient = new useQueryClient();
 
 function App() {
   
 
   return (
-    <>
+    <QueryClientProvider client={QueryClient}>
      <Router>
       <Navbar />
 
       <Routes>
         <Route path='/' element={<Home />}/>
       </Routes>
+      <Routes>
+        <Route path='/create' element={<CreateTodo />}/>
+      </Routes>
      </Router>
-    </>
+    </QueryClientProvider>
   )
 }
 
