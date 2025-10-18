de2c5a7cb58993894edda930b496e9efd014fb66
ken kagai <jibken80@gmail.com>
2025-10-18 18:44:01 +0300

    feat:  add fetch logic for fetching todos from an API and message to handle the logic for fetching success and error messages.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,44 @@
-function home() {
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+
+function Home() {
+
+    const queryClient = useQueryClient();
+
+    const fetchTodos = async () => {
+        const res = await fetch('https://stub.muindetuva.com/api/todos', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer 25|z098DRg1OmjYpQPKexZHjxEmM1o0ZVKaPYa2NDgcd3f09ba7',
+                'Content-Type': 'application/json'
+            }
+        });
+        return res.json();
+    }
+    const {data, isLoading, isError, error} = useQuery({
+        queryKey: ['todos'],
+        queryFn: fetchTodos
+    })
+
+    if (isLoading) {
+        return <p className="text-gray-500 font-semibold py-14 text-xl text-center">Loading...</p>
+    }
+    if (isError) {
+        return <p className="text-red-600 font-semibold py-14 text-xl text-center">❌ Sorry couldn't fetch your Todos! Try again.</p>
+    }
+    
     return ( 
         <div className="">
-            <h1 className="text-4xl text-center py-14 font-semibold text-blue-700">Tasks</h1>
-
+            <h1 className="text-4xl text-center py-14 font-semibold text-blue-700">TaskList</h1>
+            {
+                data?.map((todo) => (
+                    <div key={todo.id} className="w-lg  mx-auto border-gray-300 py-4 px-6 hover:bg-gray-100 transition-all duration-300 cursor-pointer">
+                        <p className="text-black font-semibold mt-2">{todo.title}</p>
+                    </div>
+                ))
+            }
             
         </div>
      );
 }
 
-export default home;
+export default Home;
