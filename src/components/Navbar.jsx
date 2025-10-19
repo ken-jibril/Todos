e906302132e338179ce806912bfd0645201b395c
ken kagai <jibken80@gmail.com>
2025-10-19 21:27:34 +0300

    update change the name of the home link to 'TaskMaster'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav className='bg-blue-900 text-white p-3 flex justify-around items-center'>
-       <Link to="/" className="font-bold text-xl ">Tasks</Link>
+       <Link to="/" className="font-bold text-xl ">TaskMaster</Link>
        <Link to="/create" className="font-bold text-xl bg-white text-blue-900 px-3 py-2 rounded-xl hover:bg-blue-500 hover:text-white transition-all duration-300">Create Todo</Link>
     </nav>
   )
