40d8944f892b404ffeab99b6facdae054575f10a
ken kagai <jibken80@gmail.com>
2025-12-01 19:16:09 +0300

    Add responsiveness to the Home page for all devices

--- src/Pages/CreateTodo.jsx
+++ src/Pages/CreateTodo.jsx
@@ -4,19 +4,13 @@ import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../firebase.config.js';
 
 function CreateTodo() {
-
   const navigate = useNavigate();
-
   const [todo, setTodo] = useState('');
   const [isCompleted, setIsCompleted] = useState(false);
   const [input, setInput] = useState('');
 
-
-  // const token = import.meta.env.VITE_API_TOKEN;
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (!todo.trim()) return alert('❌ Task cannot be empty!');
 
     const newTodo = {
@@ -28,31 +22,31 @@ function CreateTodo() {
 
     try {
       await addDoc(collection(db, 'todos'), newTodo);
-      
       setTodo('');
       setIsCompleted(false);
       setInput('');
       navigate('/');
-      
     } catch (error) {
       console.error('Error:', error);
       alert('❌ Failed to add task. Please try again.');
     }
   }
 
-return (
-  <div className="flex items-center justify-center flex-col">
-      <form onSubmit={handleSubmit} className='shadow-xl max-w-fit mx-auto mt-10 rounded-lg p-6 flex flex-col gap-4 '>
+  return (
+    <div className="flex items-center justify-center flex-col px-4 sm:px-6 md:px-8">
+      <form onSubmit={handleSubmit} className='shadow-xl max-w-md w-full mx-auto mt-10 rounded-lg p-6 flex flex-col gap-4'>
           <label htmlFor="task" className='font-semibold'>Enter New Task</label>
           <input
-          type="text"
-          placeholder='Create a new task'
-           value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-          className='w-lg px-2 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500'
+              type="text"
+              placeholder='Create a new task'
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+              className='w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500'
           />
           
-          <button type='submit' className='bg-blue-800 p-2 text-white font-semibold rounded-xl hover:bg-blue-600 transition-all duration-300 cursor-pointer'>Add Todo</button>
+          <button type='submit' className='bg-blue-800 p-2 text-white font-semibold rounded-xl hover:bg-blue-600 transition-all duration-300 w-full sm:w-auto'>
+              Add Todo
+          </button>
       </form>
     </div>
   )
